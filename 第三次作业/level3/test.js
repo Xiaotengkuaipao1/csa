2f800827107f4a4bc94a945dfc5db536e5a54346
Xiaotengkuaipao1 <92387634+Xiaotengkuaipao1@users.noreply.github.com>
2021-11-17 22:12:15 +0800

    Add files via upload

---
+++ 第三次作业/level3/test.js
@@ -0,0 +1,33 @@
+var data = {
+    age: 18,
+    name: "这是真的难",
+    education: ["小学", "初中", "高中", "大学", undefined, null],
+    friends: [
+          { name: "Amber",sex: "woman"},
+          { name: "Barbara",sex: "woman"},
+          { name: "Venti",sex: "man"}], 
+    work: { 
+            time: "2021", 
+            project: { name: "test",obtain: ["css", "html", "js"]} 
+          }, 
+    play: function() {    console.log("玩滑板");  }
+}
+
+
+
+var clone = function (obj) { 
+    if(obj === null) return null 
+    if(typeof obj !== 'object') return obj;
+    if(obj.constructor===Date) return new Date(obj); 
+    var newObj = new obj.constructor ();  //保持继承链
+    for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {   //不遍历其原型链上的属性
+            var val = obj[key];
+            newObj[key] = typeof val === 'object' ? arguments.callee(val) : val; // 使用arguments.callee解除与函数名的耦合
+        }
+    }  
+    return newObj;  
+}; 
+
+var newobj = clone(data);
+console.log(newobj);
